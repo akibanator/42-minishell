39c5beddad10079d1a72d36f6429b70cf405be83
Nevesz7 <raphael.nevesz@live.com>
2023-01-12 21:54:50 -0300

    brain.txt

--- sources/test.c
+++ sources/test.c
@@ -6,11 +6,23 @@
 /*   By: rarobert <rarobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 11:12:30 by rarobert          #+#    #+#             */
-/*   Updated: 2023/01/12 15:18:42 by rarobert         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:35:27 by rarobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	**get_path(char *envp[])
+{
+	size_t	i;
+	char	*path_arr;
+
+	i = -1;
+	while (envp[++i])
+		if (!(ft_strncmp("PATH", envp[i], 4)))
+			path_arr = envp[i];
+	return (ft_split(path_arr, ':'));
+}
 
 int	main(void)
 {
