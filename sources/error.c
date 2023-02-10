87cdef39294766b73c738670a6f18370a8038656
Nevesz7 <raphael.nevesz@live.com>
2023-02-09 22:26:44 -0300

    test -> cmd_error

--- sources/error.c
+++ sources/error.c
@@ -6,13 +6,13 @@
 /*   By: rarobert <rarobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 22:11:17 by rarobert          #+#    #+#             */
-/*   Updated: 2023/02/09 22:14:47 by rarobert         ###   ########.fr       */
+/*   Updated: 2023/02/09 22:26:14 by rarobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	test(char *print, char *error, int flag)
+void	cmd_error(char *print, char *error, int flag)
 {
 	write(2, "minishell: ", 11);
 	write(2, print, ft_strlen(print));
