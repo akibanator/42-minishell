e421bea0c3d877f6746656687faee4b50154386b
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2023-03-06 23:06:30 -0300

    refactor

--- sources/check_pipes.c
+++ sources/check_pipes.c
@@ -6,27 +6,31 @@
 /*   By: akenji-a <akenji-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:28:30 by akenji-a          #+#    #+#             */
-/*   Updated: 2023/03/06 22:20:45 by akenji-a         ###   ########.fr       */
+/*   Updated: 2023/03/06 23:05:06 by akenji-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	check_pipes(t_nelson *nelson)
+static int	check_node(char content)
 {
-	if (nelson->content[0][0] == '|')
+	if (content == '|')
 	{
 		cmd_error("syntax error near unexpected token `|'\n", "", 1);
-		return ;
+		return (1);
 	}
+	return (0);
+}
+
+void	check_pipes(t_nelson *nelson)
+{
+	if (check_node(nelson->content[0][0]))
+		return ;
 	while (nelson)
 	{
 		if (nelson->next)
 			nelson = nelson->next;
 	}
-	if (nelson->content[0][0] == '|')
-	{
-		cmd_error("syntax error near unexpected token `|'\n", "", 1);
+	if (check_node(nelson->content[0][0]))
 		return ;
-	}
 }
